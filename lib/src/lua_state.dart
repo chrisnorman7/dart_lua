6f885a834681371bbe2bf774ca3cb22157c1bde4
Chris Norman <chris.norman2@googlemail.com>
2021-11-02 22:30:31 +0000

    Bound loads of push methods.

--- lib/src/lua_state.dart
+++ lib/src/lua_state.dart
@@ -29,14 +29,6 @@ class LuaState {
   /// The pointer to use.
   final Pointer<lua_State> pointer;
 
-  /// Performs an arithmetic or bitwise operation over the two values (or one,
-  /// in the case of negations) at the top of the stack, with the value on the
-  /// top being the second operand, pops these values, and pushes the result of
-  /// the operation.
-  ///
-  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_arith).
-  void arith(int op) => lua.lua.lua_arith(pointer, op);
-
   /// Close all active to-be-closed variables in the main thread, release all
   /// objects in the given Lua state (calling the corresponding garbage-
   /// collection metamethods, if any), and frees all dynamic memory used by this
@@ -238,4 +230,174 @@ class LuaState {
     }
     return LuaState(lua, ptr);
   }
+
+  /// Converts the value at the given [index] to a generic C pointer.
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_topointer).
+  Pointer<Void>? toPointer(int index) {
+    final ptr = lua.lua.lua_topointer(pointer, index);
+    if (ptr == nullptr) {
+      return null;
+    }
+    return ptr;
+  }
+
+  /// Performs an arithmetic or bitwise operation over the two values (or one,
+  /// in the case of negations) at the top of the stack, with the value on the
+  /// top being the second operand, pops these values, and pushes the result of
+  /// the operation.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_arith).
+  void arith(int op) => lua.lua.lua_arith(pointer, op);
+
+  /// Returns 1 if the two values in indices [index1] and [index2] are
+  /// primitively equal.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_rawequal).
+  bool rawEqual(int index1, int index2) =>
+      lua.lua.lua_rawequal(pointer, index1, index2) == 1;
+
+  /// Compares two Lua values.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_compare).
+  bool compare(int index1, int index2, int op) =>
+      lua.lua.lua_compare(pointer, index1, index2, op) == 1;
+
+  /// Pushes a nil value onto the stack.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushnil).
+  void pushNil() => lua.lua.lua_pushnil(pointer);
+
+  /// Pushes a double with value [n] onto the stack.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushnumber).
+  void pushNumber(double n) => lua.lua.lua_pushnumber(pointer, n);
+
+  /// Pushes an integer with value [n] onto the stack.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushinteger).
+  void pushInteger(int n) => lua.lua.lua_pushinteger(pointer, n);
+
+  /// Pushes the string [s] onto the stack.
+  ///
+  /// Uses [DartLua.lua_pushlstring].
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushlstring).
+  Pointer<Int8> pushLString(String s) {
+    final stringPointer = s.toNativeUtf8().cast<Int8>();
+    final ptr = lua.lua.lua_pushlstring(pointer, stringPointer, s.length);
+    malloc.free(stringPointer);
+    return ptr;
+  }
+
+  /// Pushes the string [s] onto the stack.
+  ///
+  /// Uses [DartLua.lua_pushstring].
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushstring).
+  Pointer<Int8> pushString(String s) {
+    final stringPointer = s.toNativeUtf8().cast<Int8>();
+    final ptr = lua.lua.lua_pushstring(pointer, stringPointer);
+    malloc.free(stringPointer);
+    return ptr;
+  }
+
+  /// See the reference manual.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushvfstring).
+  Pointer<Int8> pushVfString(String fmt, String argp) {
+    final fmtPointer = fmt.toNativeUtf8().cast<Int8>();
+    final argpPointer = argp.toNativeUtf8().cast<Int8>();
+    final ptr = lua.lua.lua_pushvfstring(pointer, fmtPointer, argpPointer);
+    [fmtPointer, argpPointer].forEach(malloc.free);
+    return ptr;
+  }
+
+  /// Pushes onto the stack a formatted string [fmt] and returns a pointer to
+  /// this string.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushfstring).
+  Pointer<Int8> pushFString(String fmt) {
+    final fmtPointer = fmt.toNativeUtf8().cast<Int8>();
+    final ptr = lua.lua.lua_pushfstring(pointer, fmtPointer);
+    malloc.free(fmtPointer);
+    return ptr;
+  }
+
+  /// Pushes a new C closure [fn] onto the stack.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushcclosure).
+  void pushCClosure(lua_CFunction fn, int n) =>
+      lua.lua.lua_pushcclosure(pointer, fn, n);
+
+  /// Pushes a boolean value with value [b] onto the stack.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushboolean).
+  void pushBoolean(bool b) =>
+      lua.lua.lua_pushboolean(pointer, b == true ? 1 : 0);
+
+  /// Pushes a light userdata [p] onto the stack.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushlightuserdata).
+  void pushLightUserdata(Pointer<Void> p) =>
+      lua.lua.lua_pushlightuserdata(pointer, p);
+
+  /// Pushes this thread onto the stack.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_pushthread).
+  bool pushThread() => lua.lua.lua_pushthread(pointer) == 1;
+
+  /// Pushes onto the stack the value of the global [name].
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_getglobal).
+  LuaType getGlobal(String name) {
+    final namePointer = name.toNativeUtf8().cast<Int8>();
+    final value = lua.lua.lua_getglobal(pointer, namePointer);
+    malloc.free(namePointer);
+    return value.toLuaType();
+  }
+
+  /// Pushes onto the stack the value `t[k]`, where `t` is the value at the
+  /// given [index] and `k` is the value on the top of the stack.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_gettable).
+  LuaType getTable(int index) =>
+      lua.lua.lua_gettable(pointer, index).toLuaType();
+
+  /// Pushes onto the stack the value `t[k]`, where `t` is the value at the
+  /// given [index].
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_getfield).
+  LuaType getField(int index, String k) {
+    final kPointer = k.toNativeUtf8().cast<Int8>();
+    final value = lua.lua.lua_getfield(pointer, index, kPointer);
+    malloc.free(kPointer);
+    return value.toLuaType();
+  }
+
+  /// Pushes onto the stack the value `t[i]`, where `t` is the value at the
+  /// given [index].
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_geti).
+  LuaType getI(int index, int n) =>
+      lua.lua.lua_geti(pointer, index, n).toLuaType();
+
+  /// Similar to [getTable], but does a raw access (i.e., without
+  /// metamethods).
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_rawget).
+  LuaType rawGet(int index) => lua.lua.lua_rawget(pointer, index).toLuaType();
+
+  /// Pushes onto the stack the value `t[n]`, where `t` is the table at the
+  /// given [index].
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_rawgeti).
+  LuaType getRawI(int index, int n) =>
+      lua.lua.lua_rawgeti(pointer, index, n).toLuaType();
+
+  /// Pushes onto the stack the value `t[k]`, where `t` is the table at the
+  /// given [index] and `k` is the pointer [p] represented as a light userdata.
+  ///
+  /// [Lua Docs](https://www.lua.org/manual/5.4/manual.html#lua_rawgetp).
+  LuaType rawGetP(int index, Pointer<Void> p) =>
+      lua.lua.lua_rawgetp(pointer, index, p).toLuaType();
 }
